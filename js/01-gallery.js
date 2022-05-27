b5b75e9585ec167bb17210466fe942713114da30
Yuliia Kiryushina <yulio1999@gmail.com>
2022-05-27 19:23:13 +0300

    homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryContainer.addEventListener('click', clickOnGalleryContainer);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+let instance = {};
+
+function clickOnGalleryContainer(e) {
+  e.preventDefault();
+  window.addEventListener('keydown', closeOnModalEscape);
+
+  instance = basicLightbox.create(`<img class="original-image" src="${e.target.dataset.source}">`, {
+    onClose: instance => {
+      window.removeEventListener('keydown', closeOnModalEscape);
+    },
+  });
+
+  instance.show();
+}
+
+function closeOnModalEscape(e) {
+  if (e.code === 'Escape') {
+    window.removeEventListener('keydown', closeOnModalEscape);
+    instance.close();
+  }
+  console.log(e);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>
+      </li>`;
+    })
+    .join('');
+}
+const ligthbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+ligthbox.on('show.simplelightbox', () => {});
